2777ba51a2515d960c51518fcb7d51667ccd0ddd
Miten Gajjar <miten3377@gmail.com>
2020-06-14 01:01:50 +0530

    Simple UI Fixes!!
    Now users can see their password while loging in or singing up by
    taping on KEY icon.
    Now a loading screen appears when user click on LOG IN button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,26 @@
-import 'package:flash_chat/components/screens/article.dart';
-import 'package:flash_chat/components/screens/education.dart';
-import 'package:flash_chat/components/screens/home_page.dart';
-import 'package:flash_chat/components/screens/seniorpage.dart';
 import 'package:flutter/material.dart';
-import 'package:flash_chat/components/screens/login_screen.dart';
-import 'package:flash_chat/components/screens/registration_screen.dart';
-import 'package:flash_chat/components/screens/ehelp.dart';
-import 'package:flash_chat/components/screens/clothes.dart';
-import 'package:flash_chat/components/screens/health.dart';
 
+import 'components/screens/exportsScreens.dart';
 
-void main() => runApp(
-    FlashChat());
+void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      home: LoginScreen(),
-      debugShowCheckedModeBanner: false,
-      initialRoute: LoginScreen.id,
-      routes:{
-        LoginScreen.id:(context) =>LoginScreen(),
-        RegistrationScreen.id:(context) => RegistrationScreen(),
-        HomePage.id:(context) => HomePage(),
-        Education.id:(context) => Education(),
-        SeniorPage.id:(context) => SeniorPage(),
-        Article.id:(context) => Article(),
-        Ehelp.id:(context) => Ehelp(),
-        Clothes.id:(context) =>Clothes(),
-        Health.id:(context) =>Health(),
-      }
-    );
+        home: LoginScreen(),
+        debugShowCheckedModeBanner: false,
+        initialRoute: LoginScreen.id,
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+          HomePage.id: (context) => HomePage(),
+          Education.id: (context) => Education(),
+          SeniorPage.id: (context) => SeniorPage(),
+          Article.id: (context) => Article(),
+          Ehelp.id: (context) => Ehelp(),
+          Clothes.id: (context) => Clothes(),
+          Health.id: (context) => Health(),
+        });
   }
 }
-
-
- 
